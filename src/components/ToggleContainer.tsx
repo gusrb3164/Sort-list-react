4fdf110b5fce3f9bd2247f3407a76fcedeb6595d
hyun gyu <gusrb3164@gmail.com>
2021-03-08 00:06:36 +0900

    add toggle buttons

--- src/components/ToggleContainer.tsx
+++ src/components/ToggleContainer.tsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { ToggleButton, ToggleButtonGroup } from "@material-ui/lab/";
 
 const ToggleContainer: React.FC = () => {
-    return <div></div>;
+    const [formats, setFormats] = useState(["selection"]);
+
+    const handleFormat = (event: React.MouseEvent<HTMLElement>, newFormats: string[]) => {
+        setFormats(newFormats);
+        console.log(newFormats);
+    };
+    return (
+        <div className="toggle-container">
+            <ToggleButtonGroup value={formats} onChange={handleFormat} aria-label="text formatting">
+                <ToggleButton value="selection" aria-label="selection">
+                    Selection-Sort
+                </ToggleButton>
+                <ToggleButton value="insertion" aria-label="insertion">
+                    Insertion-Sort
+                </ToggleButton>
+                <ToggleButton value="bubble" aria-label="bubble">
+                    Bubble-Sort
+                </ToggleButton>
+                <ToggleButton value="quick" aria-label="quick">
+                    Quick-Sort
+                </ToggleButton>
+                <ToggleButton value="heap" aria-label="heap">
+                    Heap-Sort
+                </ToggleButton>
+                <ToggleButton value="merge" aria-label="merge">
+                    Merge-Sort
+                </ToggleButton>
+            </ToggleButtonGroup>
+        </div>
+    );
 };
 
 export default ToggleContainer;
